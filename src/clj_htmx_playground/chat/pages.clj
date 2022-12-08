aae364906eb83d94cbcd9717b95d12434be95ec5
Mark Bastian <mbastian@marks-mbp.lan>
2022-12-08 13:09:22 -0700

    Better layout

--- src/clj_htmx_playground/chat/pages.clj
+++ src/clj_htmx_playground/chat/pages.clj
@@ -22,18 +22,23 @@
 
 (defn wrap-as-page [content]
   (html5
-    (include-css "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css")
+    (include-css
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css")
     (include-js
       "https://unpkg.com/htmx.org@1.8.4"
       "https://unpkg.com/htmx.org/dist/ext/ws.js"
       "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js")
     [:style (slurp (io/resource "clj_htmx_playground/styles.css"))]
     [:style (slurp (io/resource "clj_htmx_playground/sidebars.css"))]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     content))
 
 (def landing-page
   (html5
-    (include-css "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css")
+    (include-css
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css")
     (include-js
       "https://unpkg.com/htmx.org@1.8.4"
       "https://unpkg.com/htmx.org/dist/ext/ws.js"
@@ -78,7 +83,7 @@
         "Go"]]]]]])
 
 (defn sidebar-sublist-item [label & attributes]
-  [:li [:a.link-dark.rounded
+  [:li [:a.link-dark.rounded.text-white
         (into {:id label :href "#"} attributes)
         label]])
 
@@ -101,6 +106,41 @@
    (sidebar-list "Rooms" "roomList")
    (sidebar-list "Users" "userList")])
 
+(defn navbar-sublist [& r]
+  (into [:ul.dropdown-menu] r))
+(defn navbar-list [list-name id]
+  ; Note quite there yet
+  [:li.nav-item.dropdown
+   [:a.nav-link.dropdown-toggle
+    {:href           "#"
+     :role           "button"
+     :data-bs-toggle "dropdown"}
+    list-name]
+   (navbar-sublist {:id id})]
+
+  (let [collapse-id (format "%s-collapse" id)]
+    [:li.mb-1.text-white
+     [:button.btn.btn-toggle.align-items-center.rounded.collapsed.text-white
+      {:data-bs-toggle "collapse"
+       :data-bs-target (format "#%s" collapse-id)}
+      list-name]
+     [:div.collapse
+      {:id collapse-id}
+      (sidebar-sublist {:id id})]]))
+
+(defn navbar []
+  [:nav.navbar.navbar-expand-lg.navbar-dark.bg-dark
+   [:div.container-fluid
+    [:button.navbar-toggler
+     {:type           "button"
+      :data-bs-toggle "collapse"
+      :data-bs-target "#navbarToggler"}
+     [:span.navbar-toggler-icon]]
+    [:div#navbarToggler.collapse.navbar-collapse
+     [:ul.navbar-nav.me-auto.mb-2.mb-lg-0
+      (navbar-list "Rooms" "roomList")
+      (navbar-list "Users" "userList")]]]])
+
 (defn room-change-link [room-name & attributes]
   [:a#roomChangeLink.link-primary
    (into
@@ -113,9 +153,10 @@
   (into [:div#notifications] r))
 
 (defn chat-pane [room-name]
-  [:div#chat.h-100
+  [:div#chat.overflow-scroll
+   {:style "height: calc(100vh - 100px); overflow: auto; flex-grow: 1;"}
    [:p.p-2.border
-    [:b "You are in room "]
+    [:b "You are in "]
     (room-change-link room-name)]
    room-create-modal
    [:div.p-2
@@ -123,57 +164,30 @@
 
 (defn chat-prompt
   [room-name & attributes]
-  [:input#chatPrompt.form-control
-   (into
-     {:name         "chat-message"
-      :placeholder  (format "Message #%s" room-name)
-      :autocomplete "off"}
-     attributes)])
+  [:div#chatPrompt.input-group.mb-3
+   [:input.form-control
+    (into
+      {:name         "chat-message"
+       :placeholder  (format "Message #%s" room-name)
+       :autocomplete "off"}
+      attributes)]
+   [:button.btn.btn-outline-secondary.fixed-bottom
+    {:type    "button"
+     :ws-send "true"
+     :name    "chat-message"
+     :method  :post}
+    [:i.fa.fa-paper-plane]]])
 
 (defn chat-page [{:keys [params] :as _request}]
   (let [{:keys [room-name username]} params]
     (html5
-      [:div.row.border.h-100
+      [:div.mb-3.vh-100
        {:hx-ext     "ws"
         :ws-connect (format "/ws/%s/%s" room-name username)}
-       [:div.p-2.col-xs-10.col-sm-2 (sidebar)]
-       [:div.p-2.col-xs-10.col-sm-7 (chat-pane room-name)]
-       [:form.fixed-bottom
-        {:ws-send "true" :name "chat-message" :method :post}
+       (navbar)
+       (chat-pane room-name)
+       [:form
+        {:ws-send "true"
+         :name    "chat-message"
+         :method  :post}
         (chat-prompt room-name)]])))
-
-[:nav.navbar.navbar-expand-lg.bg-light
- [:div.container-fluid
-  [:button.navbar-toggler
-   {:type           "button"
-    :data-bs-toggle "collapse"
-    :data-bs-target "#navbarTogglerDemo01"
-    :aria-controls  "navbarTogglerDemo01"}
-   [:span.navbar-toggler-icon]]
-  [:div#navbarTogglerDemo01.collapse.navbar-collapse
-   [:ul.navbar-nav.me-auto.mb-2.mb-lg-0
-    [:li.nav-item.dropdown
-     [:a.nav-link.dropdown-toggle
-      {:href           "#"
-       :role           "button"
-       :data-bs-toggle "dropdown"}
-      "Users"]
-     [:ul.dropdown-menu
-      [:li [:a.dropdown-item {:href "#"} "Action"]]
-      [:li [:a.dropdown-item {:href "#"} "Another action"]]
-      [:li [:hr.dropdown-divider]]
-      [:li [:a.dropdown-item {:href "#"} "Something else here"]]]]
-    [:li.nav-item.dropdown
-     [:a.nav-link.dropdown-toggle
-      {:href           "#"
-       :role           "button"
-       :data-bs-toggle "dropdown"}
-      "Rooms"]
-     [:ul.dropdown-menu
-      [:li [:a.dropdown-item {:href "#"} "Action"]]
-      [:li [:a.dropdown-item {:href "#"} "Another action"]]
-      [:li [:hr.dropdown-divider]]
-      [:li [:a.dropdown-item {:href "#"} "Something else here"]]]]]
-   [:form.d-flex {:role "search"}
-    [:input.form-control.me-2 {:type "search" :placeholder "Search" :aria-label "Search"}]
-    [:button.btn.btn-outline-success {:type "submit"} "Search"]]]]]
